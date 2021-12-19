b7ffd2ca70e69f0e0e8bea8bca1cca6d091b7665
Steven3761 <d.stevencr@hotmail.com>
2021-12-19 10:30:11 -0500

    proyecto lamborghini total

---
+++ JS/javascript.js
@@ -0,0 +1,14 @@
+function validar(){
+    let nombre, tipomensaje, email, comentario
+
+    nombre = document.getElementById('nombre').value
+    tipomensaje = document.getElementById('tipomensaje').value
+    email = document.getElementById('email').value
+    comentario = document.getElementById('comentario').value
+    
+
+    
+    alert(tipomensaje+" "+nombre+" "+email+" "+comentario)
+}
+    
+ 
